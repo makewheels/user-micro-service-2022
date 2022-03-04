cedf2548d3ea684e85267f6424dfbc5f3eb6d761
miuser <miuser@email.com>
2022-03-04 21:22:06 +0800

    init

---
+++ src/main/java/com/github/makewheels/usermicroservice2022/UserMicroService2022Application.java
@@ -0,0 +1,13 @@
+package com.github.makewheels.usermicroservice2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMicroService2022Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserMicroService2022Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/makewheels/usermicroservice2022/UserMicroService2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.makewheels.usermicroservice2022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserMicroService2022ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
