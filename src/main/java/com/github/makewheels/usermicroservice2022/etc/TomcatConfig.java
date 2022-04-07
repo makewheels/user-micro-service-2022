8338458f5790f92f53433e32bdf8d0c577660a38
makewheels <makewheels@github.com>
2022-04-07 18:55:51 +0800

    http port

--- src/main/java/com/github/makewheels/usermicroservice2022/etc/TomcatConfig.java
+++ src/main/java/com/github/makewheels/usermicroservice2022/etc/TomcatConfig.java
@@ -1,21 +1,32 @@
 package com.github.makewheels.usermicroservice2022.etc;
 
 import org.apache.catalina.connector.Connector;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class TomcatConfig {
 
+//    @Bean
+//    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> cookieProcessorCustomizer() {
+//        return (TomcatServletWebServerFactory factory) -> {
+//            Connector connector = new Connector();
+//            connector.setPort(5021);
+//            factory.addAdditionalTomcatConnectors(connector);
+//        };
+//    }
 
     @Bean
-    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> cookieProcessorCustomizer() {
-        return (TomcatServletWebServerFactory factory) -> {
-            Connector connector = new Connector();
-            connector.setPort(5021);
-            factory.addAdditionalTomcatConnectors(connector);
-        };
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setPort(5021);
+        tomcat.addAdditionalTomcatConnectors(connector);
+        return tomcat;
     }
+
 }
