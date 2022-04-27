cc75e2dea52fb696c977cc19a6a5ff345d9bc597
makewheels <33256842+makewheels@users.noreply.github.com>
2022-04-28 01:25:58 +0800

    111

--- src/main/java/com/github/makewheels/usermicroservice2022/user/UserController.java
+++ src/main/java/com/github/makewheels/usermicroservice2022/user/UserController.java
@@ -1,10 +1,12 @@
 package com.github.makewheels.usermicroservice2022.user;
 
 import com.github.makewheels.usermicroservice2022.response.Result;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("user")
